18e02651d675c45d9b40425c1bd2894c0b39a4df
jcisnerosp <44421246+jcisnerosp@users.noreply.github.com>
2018-11-29 13:19:20 -0500

    PYL3Parcial

--- PYL2Checkout/Ejercicio5/Class1.vb
+++ PYL2Checkout/Ejercicio5/Class1.vb
@@ -1,4 +1,12 @@
-﻿Public Class Class1
+﻿Imports System.Drawing
+Public Class Class1
     Public Function polarToCartesian(r As Double, theta As Double) As Point
+
+        If (theta = 90) Then
+            Return New Point(0, r * Math.Sin(theta))
+        Else
+            Return New Point(r * Math.Cos((theta * Math.PI) / 180), r * Math.Sin((theta * Math.PI) / 180))
+        End If
+
     End Function
 End Class
